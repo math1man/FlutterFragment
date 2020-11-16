8253ec7658030ac933d9c12c045a8b889752db9e
math1man <aweiland@google.com>
2020-11-16 11:09:39 -0800

    OnBackPressedCallback example

--- android/app/src/main/java/com/example/fragmentbug/MyActivity.java
+++ android/app/src/main/java/com/example/fragmentbug/MyActivity.java
@@ -1,35 +1,18 @@
 package com.example.fragmentbug;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import io.flutter.embedding.android.FlutterFragment;
 
 public class MyActivity extends AppCompatActivity {
-    private static final String TAG_FLUTTER_FRAGMENT = "flutter_fragment";
-    private FragmentManager fragmentManager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-        fragmentManager = getSupportFragmentManager();
-        FragmentTransaction ft = fragmentManager.beginTransaction();
-        ft.add(R.id.frameLayout, new NativeFragment());
-        ft.commit();
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (savedInstanceState == null) {
+      getSupportFragmentManager()
+          .beginTransaction()
+          .add(android.R.id.content, new MyFlutterFragment())
+          .commitNow();
     }
-
-    @Override
-    public void onBackPressed() {
-        FlutterFragment flutterFragment =
-                (FlutterFragment) fragmentManager.findFragmentByTag(TAG_FLUTTER_FRAGMENT);
-        if (flutterFragment != null) {
-            flutterFragment.onBackPressed();
-        } else {
-            super.onBackPressed();
-        }
-    }
+  }
 }
--- android/app/src/main/java/com/example/fragmentbug/MyFlutterFragment.java
+++ android/app/src/main/java/com/example/fragmentbug/MyFlutterFragment.java
@@ -1,106 +1,23 @@
 package com.example.fragmentbug;
 
-import android.os.Bundle;
-
+import android.content.Context;
 import androidx.activity.OnBackPressedCallback;
-import androidx.activity.OnBackPressedDispatcher;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentManager;
-
-import java.util.List;
-
-import io.flutter.Log;
 import io.flutter.embedding.android.FlutterFragment;
-import io.flutter.embedding.engine.systemchannels.PlatformChannel;
 
-public class MyFlutterFragment extends FlutterFragment implements PlatformChannel.PlatformMessageHandler {
-    private final static String TAG = "MyFlutterFragment";
-    private OnBackPressedCallback onBackPressedCallback;
+public class MyFlutterFragment extends FlutterFragment {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        onBackPressedCallback = new OnBackPressedCallback(true) {
-            @Override
-            public void handleOnBackPressed() {
-                Log.w(TAG, "OnBackPressedCallback Invoked");
-            }
-        };
-        getFlutterEngine().getPlatformChannel().setPlatformMessageHandler(this);
-        getActivity().getOnBackPressedDispatcher().addCallback(onBackPressedCallback);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        onBackPressedCallback.remove();
-    }
-
-    @Override
-    public void playSystemSound(@NonNull PlatformChannel.SoundType soundType) {
-
-    }
-
-    @Override
-    public void vibrateHapticFeedback(@NonNull PlatformChannel.HapticFeedbackType feedbackType) {
-
-    }
-
-    @Override
-    public void setPreferredOrientations(int androidOrientation) {
-
-    }
-
-    @Override
-    public void setApplicationSwitcherDescription(@NonNull PlatformChannel.AppSwitcherDescription description) {
-
-    }
-
-    @Override
-    public void showSystemOverlays(@NonNull List<PlatformChannel.SystemUiOverlay> overlays) {
-
-    }
-
-    @Override
-    public void restoreSystemUiOverlays() {
-
-    }
-
-    @Override
-    public void setSystemUiOverlayStyle(@NonNull PlatformChannel.SystemChromeStyle systemUiOverlayStyle) {
-
-    }
-
-    @Override
-    public void popSystemNavigator() {
-        OnBackPressedDispatcher onBackPressedDispatcher = getActivity().getOnBackPressedDispatcher();
-        if (!onBackPressedDispatcher.hasEnabledCallbacks()) {
-            onBackPressedDispatcher.onBackPressed();
-            return;
+  private final OnBackPressedCallback onBackPressedCallback =
+      new OnBackPressedCallback(true) {
+        @Override
+        public void handleOnBackPressed() {
+          onBackPressed();
         }
+      };
 
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-        if (fragmentManager.getBackStackEntryCount() > 0) {
-            fragmentManager.popBackStack();
-        } else {
-            getActivity().finish();
-        }
-    }
-
-    @Nullable
-    @Override
-    public CharSequence getClipboardData(@Nullable PlatformChannel.ClipboardContentFormat format) {
-        return null;
-    }
-
-    @Override
-    public void setClipboardData(@NonNull String text) {
-
-    }
-
-    @Override
-    public boolean clipboardHasStrings() {
-        return false;
-    }
+  @Override
+  public void onAttach(@NonNull Context context) {
+    super.onAttach(context);
+    requireActivity().getOnBackPressedDispatcher().addCallback(onBackPressedCallback);
+  }
 }
